a70ce4fce9aa0f5e20d1b1ec3c936ab3719cba89
LiloueG <liloue.guyot25@gmail.com>
2024-05-04 15:57:55 +0200

    feat : plugin vite

--- sharedreams/vite.config.ts
+++ sharedreams/vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueDevTools(),
+    VueDevTools(), 
+    Pages(),
   ],
   resolve: {
     alias: {
@@ -16,3 +18,8 @@ export default defineConfig({
     }
   }
 })
+
+
+ 
+
+
