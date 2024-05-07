0334f696c5727896b25feeb2c2268b14dfc624e6
LiloueG <liloue.guyot25@gmail.com>
2024-05-07 11:11:05 +0200

    feat :nuage

---
+++ sharedreams/src/pages/nuage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="w-[390px] h-[844px] relative overflow-hidden bg-[#090a14]">
+  <div
+    class="flex justify-center items-center absolute left-[21px] top-[697px] overflow-hidden gap-2.5 px-[90px] py-3.5 rounded-xl bg-[#00a7ee]"
+  >
+    <p class="flex-grow-0 flex-shrink-0 text-3xl font-medium text-left text-white">C’est parti !</p>
+  </div>
+  <div
+    class="w-[390px] h-[844px] absolute left-[-1px] top-[-1px]"
+    style="background: linear-gradient(to bottom, #0c0a09 0%, #4c1d95 35.5%, #7c3aed 64%, #a78bfa 100%);"
+  ></div>
+  <div class="w-[354px] h-[157px]">
+    <p class="w-[349px] h-[95px] absolute left-5 top-[505px] text-3xl text-left text-white">
+      <span class="w-[349px] h-[95px] text-3xl font-bold text-left text-white">Sharedreams, </span
+      ><br /><span class="w-[349px] h-[95px] text-3xl font-medium text-left text-white"
+        >l’endroit où tous vos rêves s’expriment</span
+      >
+    </p>
+    <p class="w-[354px] absolute left-4 top-[614px] text-base text-left text-[#efefef]">
+      L’application parfaite pour partager vos rêves avec tous vos amis !
+    </p>
+  </div>
+  <div
+    class="flex justify-center items-center absolute left-[22px] top-[691px] overflow-hidden gap-2.5 px-[90px] py-3.5 rounded-xl bg-orange-400"
+  >
+    <p class="flex-grow-0 flex-shrink-0 text-3xl font-medium text-left text-white">C’est parti !</p>
+  </div>
+</div>
+</template>
