2b34f12c9c8919f386977175bea3f242cfd9cb03
LiloueG <liloue.guyot25@gmail.com>
2024-05-23 18:07:27 +0200

    fix : new pb vers site

--- sharedreams/src/assets/backend.ts
+++ sharedreams/src/assets/backend.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase('https://sharedreams.lguyot.fr:443');
 
 export async function Adduser(event: Object) {
     try {
